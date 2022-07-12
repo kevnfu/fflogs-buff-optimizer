5fa68682ea0be23783754d0ca7eaa0703a5db201
Kevin <kevn.fu@gmail.com>
2022-07-12 01:45:42 -0500

    working api

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import oauthlib.oauth2 as oauth2
 from requests_oauthlib import OAuth2Session
-from requests.auth import HTTPBasicAuth
+# from requests.auth import HTTPBasicAuth
+import json
+from datetime import datetime
+from dataclasses import dataclass
 
 from gql import gql
 from gql import Client as GQLClient
@@ -11,22 +14,59 @@ from config import CLIENT_ID, CLIENT_SECRET
 CLIENT_API_URL = 'https://www.fflogs.com/api/v2/client'
 OAUTH_TOKEN_URL = 'https://www.fflogs.com/oauth/token'
 
-Q_FIGHTDATA = """
+Q_REPORT = """
 query {{
     reportData {{
         report(code: "{reportCode}") {{
-            # fights {{
-            #     startTime, endTime
-            # }}
-            table(startTime: 343855, endTime: 472491)
-
+            title
+            masterData {{
+                actors(subType: "Boss") {{
+                    id
+                    name
+                    subType
+                }}
+            }}
+            startTime
+            endTime
+            fights {{
+                id
+                encounterID
+                startTime
+                endTime
+            }}
         }}
     }}
 }}
 """
 
-reportCode = "19X6CgBFYLKZzMNJ"
-fightID = "50"
+Q_EVENTS = """
+query {{
+    reportData {{
+        report(code: "{reportCode}") {{
+            events(filterExpression: "inCategory('deaths') = true", startTime: 8984334, endTime: 9448023) {{
+                data
+                nextPageTimestamp
+            }}
+        }}
+    }}
+}}
+
+"""
+
+Q_ENCOUNTER= """
+query {{
+    worldData {{
+        encounter(id: {encounterID}) {{
+            zone {{
+                name
+            }}
+        }}
+    }}
+}}
+"""
+
+reportCode = "fj1tvmxZhWa2zKHd"
+fightID = "1"
 
 # auth = HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
 client = oauth2.BackendApplicationClient(CLIENT_ID)
@@ -38,7 +78,7 @@ token = session.fetch_token(
     client_secret=CLIENT_SECRET
 )
 
-print(token)
+# print(token)
 
 access_token = token['access_token']
 
@@ -47,8 +87,71 @@ transport.headers = {'Authorization': f'Bearer {access_token}'}
 
 gql_client = GQLClient(transport=transport, fetch_schema_from_transport=True)
 
-gql_q = gql(Q_FIGHTDATA.format(reportCode=reportCode, fightID=fightID))
+gql_q = gql(Q_REPORT.format(reportCode=reportCode))
 
-x = gql_client.execute(gql_q)
+res = gql_client.execute(gql_q)
 
-print(x)
+report = res['reportData']['report']
+
+bosses = report['masterData']['actors']
+startTime = report['startTime']
+endTime = report['endTime']
+fights = report['fights']
+
+for fight in fights:
+    print(fight)
+
+gql_q = gql(Q_ENCOUNTER.format(encounterID=1065))
+res = gql_client.execute(gql_q)
+print(res)
+
+
+# print(json.dumps(report, indent=4))
+time = report['startTime'] / 1000 + 18016083/1000
+# print(time)
+print(datetime.fromtimestamp(time).strftime('%H:%M:%S'))
+
+@dataclass 
+class Report:
+    code: str
+    title: str
+    startTime: int
+    endTime: int
+    bosses: dict()
+    fights: list()
+
+@dataclass
+class Fight:
+    """docstring for Fight"""
+    i: int
+    encounterID: int
+    startTime: int
+    endTime: int
+    # report: Report
+
+    def __init__(self, json):
+        self.i = json['id']
+        self.encounterID = json['encounterID']
+        self.startTime = json['startTime']
+        self.endTime = json['endTime']
+
+
+@dataclass
+class Bosses:
+    code: int
+    name: str
+
+
+def get_report(code):
+    res = gql_client.execute(gql(Q_REPORT.format(reportCode=reportCode)))
+    report = res['reportData']['report']
+    return Report(
+        code=code,
+        title=report['title'],
+        startTime=report['startTime'],
+        endTime=report['endTime'],
+        bosses=report['masterData']['actors'],
+        fights=list(map(Fight, report['fights']))
+    )
+
+print(get_report(reportCode))
