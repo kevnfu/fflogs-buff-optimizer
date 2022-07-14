f9b7e4653fdee2f301b9734fe4a1466d3ddab32e
Kevin <kevn.fu@gmail.com>
2022-07-14 03:42:26 -0500

    refactor

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from requests_oauthlib import OAuth2Session
 import json
 from datetime import datetime
 from dataclasses import dataclass
-
+from enum import Enum
 from math import floor
 
 from gql import gql
@@ -12,9 +12,6 @@ from gql import Client as GQLClient
 from gql.transport.requests import RequestsHTTPTransport
 
 from config import CLIENT_ID, CLIENT_SECRET
-
-CLIENT_API_URL = 'https://www.fflogs.com/api/v2/client'
-OAUTH_TOKEN_URL = 'https://www.fflogs.com/oauth/token'
 
 Q_REPORT = """
 query {{
@@ -26,15 +23,18 @@ query {{
         report(code: "{reportCode}") {{
             title
             masterData {{
-                actors(subType: "Boss") {{
+                bosses: actors(subType: "Boss") {{
                     id
                     name
-                    subType
+                }}
+                players: actors(type: "Player") {{
+                    id
+                    name
                 }}
             }}
             startTime
             endTime
-            fights {{
+            fights (encounterID: {encounterID}) {{
                 id
                 encounterID
                 startTime
@@ -52,7 +52,7 @@ query {{
     reportData {{
         report(code: "{reportCode}") {{
             # filterExpression: "inCategory('deaths')=true"
-            events(dataType: Deaths, hostilityType: Enemies, limit: 10000, startTime: {startTime}, endTime: {endTime}) {{
+            events(dataType: Deaths, limit: 10000, startTime: {startTime}, endTime: {endTime}) {{
                 data
                 nextPageTimestamp
             }}
@@ -67,7 +67,8 @@ query {{
     reportData {{
         report(code: "{reportCode}") {{
             events(dataType: DamageDone, filterExpression: "overkill > 0",
-                hostilityType: Friendlies, limit: 10000, startTime: {startTime}, endTime: {endTime}) {{
+                hostilityType: Friendlies, limit: 10000, 
+                startTime: {startTime}, endTime: {endTime}) {{
                 data
                 nextPageTimestamp
             }}
@@ -77,7 +78,26 @@ query {{
 
 """
 
-Q_ENCOUNTER= """
+Q_DEATHS_OVERKILLS = """
+query {{
+    reportData {{
+        report(code: "{reportCode}") {{
+            deaths: events(dataType: Deaths, limit: 10000, startTime: {startTime}, endTime: {endTime}) {{
+                data
+                nextPageTimestamp
+            }}
+            overkills: events(dataType: DamageDone, filterExpression: "overkill > 0",
+                hostilityType: Friendlies, limit: 10000, 
+                startTime: {startTime}, endTime: {endTime}) {{
+                data
+                nextPageTimestamp
+            }}
+        }}
+    }}
+}}
+"""
+
+Q_ENCOUNTER = """
 query {{
     worldData {{
         encounter(id: {encounterID}) {{
@@ -89,29 +109,8 @@ query {{
 }}
 """
 
-# reportCode = "XZdRqnWAPBGQf3jg"
-# reportCode = "7WtwbQyvpFDA2RhZ"
-reportCode = "rHq4kRnmKcbpgwZY"
-
-# auth = HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-client = oauth2.BackendApplicationClient(CLIENT_ID)
-session = OAuth2Session(client=client)
-
-token = session.fetch_token(
-    OAUTH_TOKEN_URL,
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET
-)
-
-access_token = token['access_token']
-
-transport = RequestsHTTPTransport(url=CLIENT_API_URL)
-transport.headers = {'Authorization': f'Bearer {access_token}'}
-
-gql_client = GQLClient(transport=transport, fetch_schema_from_transport=True)
-
-# time = report['startTime'] / 1000 + 18016083/1000
-# print(datetime.fromtimestamp(time).strftime('%H:%M:%S'))
+class Encounter(Enum):
+    DSU = 1065
 
 @dataclass 
 class Report:
@@ -120,7 +119,11 @@ class Report:
     startTime: int
     endTime: int
     bosses: dict()
+    players: dict()
     fights: list()
+    overkills: list()
+    deaths: list()
+
 
 @dataclass
 class Fight:
@@ -133,19 +136,66 @@ class Fight:
     lastPhase: int
     # report: Report
 
-    def __init__(self, json):
-        self.i = json['id']
-        self.encounterID = json['encounterID']
-        self.startTime = json['startTime']
-        self.endTime = json['endTime']
-        self.percent = json['fightPercentage']
-        self.lastPhase = json['lastPhaseAsAbsoluteIndex']
+class FFClient:
+    CLIENT_API_URL = 'https://www.fflogs.com/api/v2/client'
+    OAUTH_TOKEN_URL = 'https://www.fflogs.com/oauth/token'
 
+    def __init__(self, client_id: str, secret: str) -> None:
+        client = oauth2.BackendApplicationClient(client_id)
+        session = OAuth2Session(client=client)
+        token = session.fetch_token(
+            self.OAUTH_TOKEN_URL,
+            client_id=client_id,
+            client_secret=secret
+        )
 
-@dataclass
-class Bosses:
-    code: int
-    name: str
+        self.access_token = token['access_token']
+
+        transport = RequestsHTTPTransport(url=self.CLIENT_API_URL)
+        transport.headers = {'Authorization': f'Bearer {self.access_token}'}
+
+        self.client = GQLClient(transport=transport, fetch_schema_from_transport=True)
+
+    def q(self, query: str):
+        return self.client.execute(gql(query))
+
+    def report(self, code: str) -> Report:
+        res = self.q(Q_REPORT.format(reportCode=reportCode,encounterID=Encounter.DSU.value))
+        report = res['reportData']['report']
+
+        master = report['masterData']
+        bosses = {actor['id']: actor['name'] for actor in master['bosses']}
+        players = {actor['id']: actor['name'] for actor in master['players']}
+
+        res = self.q(Q_DEATHS_OVERKILLS.format(
+            reportCode=code,
+            startTime=report['fights'][0]['startTime'],
+            endTime=report['fights'][-1]['endTime']))
+        overkills = res['reportData']['report']['overkills']['data']
+        deaths = res['reportData']['report']['deaths']['data']
+
+        fights = list()
+        for fight in report['fights']:
+            fights.append(Fight(
+                i=fight['id'],
+                encounterID=fight['encounterID'],
+                startTime=fight['startTime'],
+                endTime=fight['endTime'],
+                percent=fight['fightPercentage'],
+                lastPhase=fight['lastPhaseAsAbsoluteIndex']
+            ))
+
+        return Report(
+            code=code,
+            title=report['title'],
+            startTime=report['startTime'],
+            endTime=report['endTime'],
+            bosses=bosses,
+            players=players,
+            fights=fights,
+            overkills=overkills,
+            deaths=deaths
+        )
 
 @dataclass
 class Deaths:
@@ -157,26 +207,13 @@ class Deaths:
     def __init__(self, json, report):
         self.code = json['targetID']
         self.time = json['timestamp']
-        self.name = report.bosses[self.code]
+        self.name = report.bosses.get(self.code, str(self.code))
         self.fightID = json['fight']
 
     def __str__(self):
         return f'{self.time}: {self.name} died Pull: {self.fightID}'
 
-def get_report(code):
-    res = gql_client.execute(gql(Q_REPORT.format(reportCode=reportCode)))
-    # print(json.dumps(res, indent=4))
-    report = res['reportData']['report']
-    bosses = dict()
-    bosses = {actor['id']: actor['name'] for actor in report['masterData']['actors'] if actor['subType'] == 'Boss'}
-    return Report(
-        code=code,
-        title=report['title'],
-        startTime=report['startTime'],
-        endTime=report['endTime'],
-        bosses=bosses,
-        fights=list(map(Fight, report['fights']))
-    )
+
 
 def from_ms(ms: int) -> str:
     hour = floor(ms/(1000*60*60) % 24)
@@ -184,33 +221,21 @@ def from_ms(ms: int) -> str:
     second = floor(ms/(1000) % 60)
     return f'{hour:02.0f}:{minute:02.0f}:{second:02.0f}'
 
-def to_ms(hour, minute, second) -> int:
-    return hour*1000*60*60 + minute*1000*60 + second*1000
-
-def get_death_events(report):
-    res = gql_client.execute(gql(Q_DEATHS.format(
-        reportCode=report.code,
-        startTime=report.fights[0].startTime,
-        endTime=report.fights[-1].endTime)))
-    # print(res)
-    # {10: 'Ser Adelphel', 11: 'Ser Grinnaux', 15: 'Ser Charibert', 22: 'spear of the Fury', 23: 'King Thordan', 39: 'Nidhogg', 42: 'left eye', 43: 'right eye', 52: 'King Thordan'}
-    for event in res['reportData']['report']['events']['data']:
-        print(str(Deaths(event, report)))
+def to_ms(timestr: str) -> int:
+    minutes, seconds = timestr.split(':')
+    return int(minutes)*1000*60 + int(seconds)*1000
 
 def print_timestamps(report, timestr, ref_fight=0):
-    minutes, seconds = timestr.split(':')
-    offset = (int(minutes)*60 + int(seconds))*1000 - report.fights[ref_fight].startTime
+    offset = to_ms(timestr) - report.fights[ref_fight].startTime
     print('00:00 Start')
     for fight in report.fights:
         # print(from_ms(fight.startTime-report.fights[0].startTime))
-        if fight.encounterID != 1065: continue
         print(f'{from_ms(fight.startTime + offset)} Pull {fight.i} Phase {fight.lastPhase}')
 
     print(f'\nhttps://www.fflogs.com/reports/{report.code}')
 
 def print_twitch(report, url, timestr, ref_fight=0):
-    minutes, seconds = timestr.split(':')
-    offset = (int(minutes)*60 + int(seconds))*1000 - report.fights[ref_fight].startTime
+    offset = to_ms(timestr) - report.fights[ref_fight].startTime
     for fight in report.fights:
         # print(from_ms(fight.startTime-report.fights[0].startTime))
         if fight.encounterID != 1065: continue
@@ -219,27 +244,19 @@ def print_twitch(report, url, timestr, ref_fight=0):
         seconds = floor(ms/(1000) % 60)
         print(f'{url}?t={minutes}m{seconds}s Pull {fight.i} Phase {fight.lastPhase} ')
 
-def get_overkill(report):
-    res = gql_client.execute(gql(Q_OVERKILL.format(
-        reportCode=report.code,
-        startTime=report.fights[0].startTime,
-        endTime=report.fights[-1].endTime)))
+def fight_phases(report):
+    print(report.overkills)
 
-    data = res['reportData']['report']['events']['data']
+# reportCode = "XZdRqnWAPBGQf3jg"
+# reportCode = "7WtwbQyvpFDA2RhZ"
+reportCode = "rHq4kRnmKcbpgwZY"
+# reportCode = "PkaMGFgDf7hBWn2p"
 
-    for event in data:
-        # if event['targetID'] in report.bosses:
-        print(report.bosses[event['targetID']])
-        print(event)
+client = FFClient(CLIENT_ID, CLIENT_SECRET)
+report = client.report(reportCode)
 
-report = get_report(reportCode)
-# print_timestamps(report, '07:35')
-# print_twitch(report, 'https://www.twitch.tv/videos/1530252131', '02:02')
+fight_phases(report)
 
-get_overkill(report)
-
-# print(report.bosses)
-# get_events(report)
 
 # phase 1 start -> Ser Adelphel/Grinnaux gone -> Pure of heart damage?
 # phase 2 start -> King thordan overkill
