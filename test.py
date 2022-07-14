c751811ec74dd7035adfd235865e7fe06c30d224
Kevin <55dRxiQVkt4h3rZ>
2022-07-14 18:39:25 -0500

    adding phases

--- test.py
+++ test.py
@@ -14,41 +14,46 @@ from gql.transport.requests import RequestsHTTPTransport
 from config import CLIENT_ID, CLIENT_SECRET
 
 Q_REPORT = """
-query {{
-    rateLimitData {{
+query Report ($reportCode: String!, $encounterID: Int) {
+    rateLimitData {
         limitPerHour
         pointsSpentThisHour
-    }}
-    reportData {{
-        report(code: "{reportCode}") {{
+    }
+    reportData {
+        report(code: $reportCode) {
             title
-            masterData {{
-                bosses: actors(subType: "Boss") {{
+            masterData {
+                bosses: actors(subType: "Boss") {
                     id
                     name
-                }}
-                players: actors(type: "Player") {{
+                }
+                players: actors(type: "Player") {
                     id
                     name
-                }}
-            }}
+                }
+                abilities {
+                    gameID
+                    name
+                    # type
+                }
+            }
             startTime
             endTime
-            fights (encounterID: {encounterID}) {{
+            fights (encounterID: $encounterID) {
                 id
                 encounterID
                 startTime
                 endTime
                 fightPercentage
                 lastPhaseAsAbsoluteIndex
-            }}
-        }}
-    }}
-}}
+            }
+        }
+    }
+}
 """
 
 Q_DEATHS = """
-query {{
+query Deaths {{
     reportData {{
         report(code: "{reportCode}") {{
             # filterExpression: "inCategory('deaths')=true"
@@ -79,22 +84,26 @@ query {{
 """
 
 Q_DEATHS_OVERKILLS = """
-query {{
-    reportData {{
-        report(code: "{reportCode}") {{
-            deaths: events(dataType: Deaths, limit: 10000, startTime: {startTime}, endTime: {endTime}) {{
+query DeathsAndOverkills ($reportCode: String!, $startTime: Float, $endTime: Float) {
+    rateLimitData {
+        limitPerHour
+        pointsSpentThisHour
+    }
+    reportData {
+        report(code: $reportCode) {
+            deaths: events(dataType: Deaths, limit: 10000, startTime: $startTime, endTime: $endTime) {
                 data
                 nextPageTimestamp
-            }}
+            }
             overkills: events(dataType: DamageDone, filterExpression: "overkill > 0",
                 hostilityType: Friendlies, limit: 10000, 
-                startTime: {startTime}, endTime: {endTime}) {{
+                startTime: $startTime, endTime: $endTime) {
                 data
                 nextPageTimestamp
-            }}
-        }}
-    }}
-}}
+            }
+        }
+    }
+}
 """
 
 Q_ENCOUNTER = """
@@ -112,18 +121,8 @@ query {{
 class Encounter(Enum):
     DSU = 1065
 
-@dataclass 
-class Report:
-    code: str
-    title: str
-    startTime: int
-    endTime: int
-    bosses: dict()
-    players: dict()
-    fights: list()
-    overkills: list()
-    deaths: list()
-
+def print_json(x):
+    print(json.dumps(x, indent=4))
 
 @dataclass
 class Fight:
@@ -135,6 +134,30 @@ class Fight:
     percent: float
     lastPhase: int
     # report: Report
+
+    def __init__(self, data: dict) -> None:
+        self.i=data['id']
+        self.encounterID=data['encounterID']
+        self.startTime=data['startTime']
+        self.endTime=data['endTime']
+        self.percent=data['fightPercentage']
+        self.lastPhase=data['lastPhaseAsAbsoluteIndex']
+
+@dataclass
+class Death:
+    targetID: int
+    time: int
+    fight: int
+    sourceID: int
+    abilityID: int
+
+    def __init__(self, data: dict) -> None:
+        self.time=data['timestamp']
+        self.sourceID=data['sourceID']
+        self.targetID=data['targetID']
+        self.fight=data['fight']
+        self.abilityID=data['abilityGameID']
+
 
 class FFClient:
     CLIENT_API_URL = 'https://www.fflogs.com/api/v2/client'
@@ -149,71 +172,63 @@ class FFClient:
             client_secret=secret
         )
 
-        self.access_token = token['access_token']
+        self._token = token['access_token']
 
         transport = RequestsHTTPTransport(url=self.CLIENT_API_URL)
-        transport.headers = {'Authorization': f'Bearer {self.access_token}'}
+        transport.headers = {'Authorization': f'Bearer {self._token}'}
 
-        self.client = GQLClient(transport=transport, fetch_schema_from_transport=True)
+        self._client = GQLClient(transport=transport, fetch_schema_from_transport=True)
 
-    def q(self, query: str):
-        return self.client.execute(gql(query))
+    def q(self, query: str, params: dict) -> 'json':
+        res = self._client.execute(gql(query), variable_values=params)
+        # print(res['rateLimitData'])
+        return res
 
-    def report(self, code: str) -> Report:
-        res = self.q(Q_REPORT.format(reportCode=reportCode,encounterID=Encounter.DSU.value))
+class Report:
+    def __init__(self, code: str, client: 'FFClient') -> None:
+        self._client = client
+        self._code = code
+        self.fetch_data()
+
+    def fetch_data(self) -> 'self':
+        res = self._client.q(Q_REPORT, {
+            'reportCode': self._code,
+            'encounterID': Encounter.DSU.value})
+
         report = res['reportData']['report']
 
-        master = report['masterData']
-        bosses = {actor['id']: actor['name'] for actor in master['bosses']}
-        players = {actor['id']: actor['name'] for actor in master['players']}
+        # master data
+        self.bosses = {actor['id']: actor['name'] for actor in report['masterData']['bosses']}
+        self.players = {actor['id']: actor['name'] for actor in report['masterData']['players']}
+        self.abilities = {ability['gameID']: ability['name'] for ability in report['masterData']['abilities']}
 
-        res = self.q(Q_DEATHS_OVERKILLS.format(
-            reportCode=code,
-            startTime=report['fights'][0]['startTime'],
-            endTime=report['fights'][-1]['endTime']))
+        # fight data
+        self.fights = [Fight(data) for data in report['fights']]
+
+        # deaths and overkills
+        res = self._client.q(Q_DEATHS_OVERKILLS, {
+            'reportCode': self._code,
+            'startTime': self.fights[0].startTime,
+            'endTime': self.fights[-1].endTime
+            })
+
         overkills = res['reportData']['report']['overkills']['data']
         deaths = res['reportData']['report']['deaths']['data']
+        # print_json(deaths)
+        self.overkills = list(map(Death, overkills))
+        self.deaths = list(map(Death, deaths))
 
-        fights = list()
-        for fight in report['fights']:
-            fights.append(Fight(
-                i=fight['id'],
-                encounterID=fight['encounterID'],
-                startTime=fight['startTime'],
-                endTime=fight['endTime'],
-                percent=fight['fightPercentage'],
-                lastPhase=fight['lastPhaseAsAbsoluteIndex']
-            ))
+        return self
 
-        return Report(
-            code=code,
-            title=report['title'],
-            startTime=report['startTime'],
-            endTime=report['endTime'],
-            bosses=bosses,
-            players=players,
-            fights=fights,
-            overkills=overkills,
-            deaths=deaths
-        )
+    def relative_time(self, event):
+        return
 
-@dataclass
-class Deaths:
-    code: int
-    time: int
-    name: str
-    fightID: int
-
-    def __init__(self, json, report):
-        self.code = json['targetID']
-        self.time = json['timestamp']
-        self.name = report.bosses.get(self.code, str(self.code))
-        self.fightID = json['fight']
-
-    def __str__(self):
-        return f'{self.time}: {self.name} died Pull: {self.fightID}'
-
-
+    def fight_phases(self):
+        for fight in self.fights:
+            # print(fight.i)
+            boss_deaths = filter(lambda x: x.fight==fight.i, self.overkills)
+            boss_names = list(map(lambda x: self.bosses[x.targetID], boss_deaths))
+            print(boss_names)
 
 def from_ms(ms: int) -> str:
     hour = floor(ms/(1000*60*60) % 24)
@@ -244,8 +259,7 @@ def print_twitch(report, url, timestr, ref_fight=0):
         seconds = floor(ms/(1000) % 60)
         print(f'{url}?t={minutes}m{seconds}s Pull {fight.i} Phase {fight.lastPhase} ')
 
-def fight_phases(report):
-    print(report.overkills)
+
 
 # reportCode = "XZdRqnWAPBGQf3jg"
 # reportCode = "7WtwbQyvpFDA2RhZ"
@@ -253,9 +267,13 @@ reportCode = "rHq4kRnmKcbpgwZY"
 # reportCode = "PkaMGFgDf7hBWn2p"
 
 client = FFClient(CLIENT_ID, CLIENT_SECRET)
-report = client.report(reportCode)
+report = Report(reportCode, client)
 
-fight_phases(report)
+report.fight_phases()
+
+# print(report.deaths)
+with open('output.txt', 'w') as f:
+    f.write(json.dumps(report.abilities, indent=4))
 
 
 # phase 1 start -> Ser Adelphel/Grinnaux gone -> Pure of heart damage?
